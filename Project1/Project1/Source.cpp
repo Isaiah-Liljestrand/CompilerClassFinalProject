3ff09ea69c7af30c35662c21c373abe78fe3f053
biopox <benjamin.fowler@student.nmt.edu>
2019-01-16 14:54:14 -0700

    C++ framework
    Added c++ framework and the char to the main class.

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Hello, World!";
+	return 0;
+}
+
+int size(string input)
+{
+	int i = 0, count;
+	for (i = 0; inut[i]; i++) {
+		count++;
+	}
+	return count;
+}
