1a54345da2f39fb965afe3c893312d207b2afec1
biopox <benjamin.fowler@student.nmt.edu>
2019-01-18 20:26:35 -0700

    Java files
    adding eclipse java for language switch

---
+++ Compiler/src/front/Scanner.java
@@ -0,0 +1,18 @@
+package front;
+
+public class Scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int count(String check){
+		int i = 0, cnt = 0;
+		for(i = 0; i < check.length(); i++){
+			cnt++;
+		}
+		return cnt;
+	}
+
+}
