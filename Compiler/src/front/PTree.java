c038bd790b083a6fc23edd2ddac7bf95ed5b6ad5
biopox <benjamin.fowler@student.nmt.edu>
2019-02-10 20:18:58 -0700

    Fixed PTree (parse tree), made Grammar use it

--- Compiler/src/front/PTree.java
+++ Compiler/src/front/PTree.java
@@ -1,7 +1,14 @@
 package front;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Parse Tree Class.
+ * Keeps track of the hierarchy order of the parsing
+ * @author Ben
+ *
+ */
 public class PTree {
 	private type_enum type;
 	private List<PTree> children;
@@ -57,28 +64,46 @@ public class PTree {
 		IDfunc //not actualy sure what this is...
 	}
 	
-	
+	/**
+	 * basic constructor
+	 */
 	PTree(){
-		
+		children = new ArrayList<PTree>();
 	}
 
+	/** 
+	 * constructor with a child tree argument
+	 * @param treein
+	 */
 	PTree(PTree treein){
+		children = new ArrayList<PTree>();
 		children.add(treein);
 	}
 	
+	/**
+	 * constructor with a type argument
+	 */
 	PTree(String intype){
+		children = new ArrayList<PTree>();
 		this.type = type_enum.valueOf(intype);
 	}
 	
+	/**
+	 * constructor with a type & child tree argument
+	 */
 	PTree(String intype, PTree treein){
+		children = new ArrayList<PTree>();
 		this.type = type_enum.valueOf(intype);
 		children.add(treein);
 	}
 	
-	
+	/**
+	 * prints this branch's type and all of it's children's type
+	 */
 	public void printTree(){
-		System.out.println("\nHash Tree:");
-		PT(0); //assumes the calling PTree is root
+		System.out.print("\nHash Tree:");
+		PT(0);
+		System.out.println();
 	}
 	
 	/**
@@ -88,19 +113,26 @@ public class PTree {
 	 * @param i the depth of the current child
 	 */
 	private void PT(int i){
-		if(!children.isEmpty()){
-			int j = 0;
+		int j = 0;
+		if(this.getType() != type_enum.valueOf("root")){
 			for(j = 0; j < i; j++){
 				System.out.print("  ");
 			}
 			System.out.print("[" + this.type);
-	
+		}
+		else{
+			i -= 1;
+		}
+		if(this.children != null){
 			for(j = 0; j < this.children.size(); j++){
 				System.out.println();
 				children.get(j).PT(i+1);
 			}
+		}
+		if(this.getType() != type_enum.valueOf("root")){
 			System.out.print("]");
 		}
+		
 	}
 
 	/**
@@ -121,12 +153,11 @@ public class PTree {
 	 * @param treein a single child PTree to add to this class' list
 	 */
 	protected void addChild(PTree treein){
-		if(treein == null){ //makes it work, despite that it should be the opposite 
-			children.add(treein);
+		if(treein != null){
+			this.children.add(treein);
+			//System.out.println("made it here");
 		}
-		else{
-			
-		}
+
 	}
 	
 	/**
@@ -147,6 +178,10 @@ public class PTree {
 		return type;
 	}
 	
+	/**
+	 * Sets the type
+	 * @param intype
+	 */
 	public void setType(String intype){
 		type = type_enum.valueOf(intype);
 	}
