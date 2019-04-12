da30d7e62cf8bc5c9a36d2ef0f99f3c7e78ed78a
Chris <christopher.fotnesbarry@student.nmt.edu>
2019-04-12 00:49:15 -0600

    Updated VarElement and added functions to VarList

--- Compiler/src/CompilerCode/VarElement.java
+++ Compiler/src/CompilerCode/VarElement.java
@@ -1,11 +1,22 @@
 package CompilerCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VarElement {
 	public String name;
-	public String location;
+	public List<String> location = new ArrayList<String>();
 	
 	public VarElement(String name, String location) {
 		this.name = name;
-		this.location = location;
+		this.location.add(location);
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public List<String> getLocation() {
+		return this.location;
 	}
 }
--- Compiler/src/CompilerCode/VarList.java
+++ Compiler/src/CompilerCode/VarList.java
@@ -9,4 +9,43 @@ public class VarList {
 	public void addElement(String name, String location) {
 		list.add(new VarElement(name, location));
 	}
+	
+	public void removeElement(String Name) {
+		int i = 0;
+		for(VarElement var : list) {
+			if(var.getName() == Name) {
+				list.remove(i);
+				break;
+			}
+			i++;
+		}
+	}
+	
+	public List<String> varLocation(String Name) {
+		for(VarElement var : list) {
+			if(var.getName() == Name) {
+				return var.getLocation();
+			}
+		}
+		return null;
+	}
+	
+	public String locVariable(String Location) {
+		for(VarElement var : list) {
+			for(String loc : var.getLocation()) {
+				if(loc == Location) {
+					return var.getName();
+				}
+			}
+		}
+		return null;
+	}
+	
+	public void changeLocation(String Name, List<String> Locations) {
+		for(VarElement var : list) {
+			if(var.getName() == Name) {
+				var.location = Locations;
+			}
+		}
+	}
 }
