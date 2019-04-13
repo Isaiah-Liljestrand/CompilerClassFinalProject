362f734e15b6f914f13e9c6ac8c79db0590a194f
biopox <benjamin.fowler@student.nmt.edu>
2019-04-12 22:25:41 -0600

    New Usage, old left as comment if needed later

--- Compiler/src/CompilerCode/RegStack.java
+++ Compiler/src/CompilerCode/RegStack.java
@@ -1,13 +1,181 @@
 package CompilerCode;
 
 import java.util.ArrayList;
-
 /**
  * Keeps track of the values on the stack and in the registers.
  * @author Ben
  *
  */
 public class RegStack {
+	int regNum;
+	private ArrayList<Integer> stack = new ArrayList<Integer>();
+	
+	
+	RegStack(){
+		this.regNum = 14; //rsp & rbp not touched
+		setup(regNum);
+	}
+	
+	RegStack(int regNum){
+		this.regNum = regNum;
+		setup(regNum);
+	}
+	
+	/**
+	 * sets adds the number of free registers
+	 * @param regNum
+	 */
+	private void setup(int regNum){
+		int i = 0;
+		for(i = 0; i < regNum; i++){
+			stack.add(0);
+		}
+		//sets the stack and base to never be used for standard operations
+	}
+	
+	/**
+	 * gives user the next free reg or a stack space if no free reg exist
+	 * @return
+	 */
+	public int giveMeReg(){
+		int LRU = findLRUReg();
+		if(stack.get(LRU) == 0){ //reg is free!
+			stack.set(LRU, 1);
+			return LRU;
+		}
+		else{ //LRU reg is not free :(
+			//use isRegEmpty next time
+			//auto pushing to stack
+			return nextFreeSpace();
+		}
+	}
+	
+	/**
+	 * 
+	 * @return free reg or lest used reg if no free reg exist
+	 */
+	public int findLRUReg(){
+		int holder = 0;
+		for(int i = 0; i < regNum; i++){
+			if(stack.get(i) == 0){ //free reg!
+				return i;
+			}
+			if(stack.get(i) > stack.get(holder)){ //finds largest non 0 (not free) reg
+				holder = i;
+			}
+		}
+		return holder;
+	}
+	
+	/**
+	 * rename of isRegEmpty for useability
+	 * @return
+	 */
+	public boolean isThereEmptyReg(){
+		return isRegEmpty();
+	}
+	
+	/**
+	 * checks if the LRU reg is free (0) (if there is a free reg)
+	 * @return
+	 */
+	public boolean isRegEmpty(){
+		int tmp = stack.get(this.findLRUReg());
+		if(tmp == 0){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * checks if reg is free (0)
+	 * @param reg
+	 * @return
+	 */
+	public boolean isRegEmpty(int reg){
+		int tmp = stack.get(reg);
+		if(tmp == 0){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * uses the next reg and frees the reg
+	 * @param reg
+	 * @return
+	 */
+	public int pushToStack(int reg){
+		int tmp = nextFreeSpace();
+		stack.set(tmp, 0);
+		return tmp;
+	}
+	
+	/**
+	 * increments the LRU, sets the used register to 1, for optimization
+	 * @param num
+	 */
+	public void usedReg(int num){
+		incrementLRU();
+		stack.set(num, 1);
+	}
+	
+	/**
+	 * increments the LRU, we used a register, for optimization
+	 */
+	private void incrementLRU(){
+		int i = 0;
+		for(i = 0; i < regNum; i++){
+			if(stack.get(i) != 0){ //increments the used regs
+				stack.set(i, stack.get(i)+1);
+			}
+		}
+	}
+	
+	/**
+	 * uses the next free space
+	 * @return the int in stack space of that space
+	 */
+	public int nextFreeSpace(){ //on stack
+		for(int i = regNum; i < stack.size(); i++){
+			if(stack.get(i) == 0){ //free spot already allocated stack				
+				stack.set(i, 1); //marks as now used space
+				return i;
+			}
+		}
+		stack.add(1); //no already allocated stack spaces are free
+		return stack.size();
+	}
+	
+	/**
+	 * give effectively rsp size
+	 */
+	public int stackSize(){
+		return stack.size()-regNum;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Misunderstood group goals for this class
+//keeping as functionality is actually in this class, just alot of other crap
+/** public class RegStack {
 	int regNum;
 	private ArrayList<Integer> stack;
 	private ArrayList<Integer> lastUsed;
@@ -28,7 +196,7 @@ public class RegStack {
 		r14 (),
 		r15 ()
 	};
-	*/
+	
 	
 	RegStack(){
 		this.regNum = 16;
@@ -57,7 +225,7 @@ public class RegStack {
 	
 	/**
 	 * useful for selecting a reg with setReg 
-	 */
+	
 	public int findLRUReg(){
 		int holder = 0;
 		for(int i = 0; i < regNum; i++){
@@ -75,11 +243,11 @@ public class RegStack {
 	 * @param reg
 	 * @param value
 	 * @return stack value if non empty (0 value) register
-	 */
+	
 	public int setReg(int reg, int value){
 		/**
 		 * change reg value on AR here
-		 */
+		
 		if(stack.get(reg) != 0){ //non empty register 
 			return pushRegToStack(reg);
 		}
@@ -93,18 +261,18 @@ public class RegStack {
 	 * if you don't care what reg it's using
 	 * @param value
 	 * @return
-	 */
+	 
 	public int setReg(int value){
 		/**
 		 * change reg value on AR here
-		 */
+		 
 		return setReg(findLRUReg(), value);
 	}
 
 	/**
 	 * hotfix to the "loss of a variable if pushed to the stack while trying to push to reg" problem (can't return 2 ints)
 	 * @return true if the LRU reg is empty (0)
-	 */
+	 
 	public boolean isRegEmpty(){
 		int tmp = stack.get(this.findLRUReg());
 		if(tmp == 0){
@@ -117,7 +285,7 @@ public class RegStack {
 	 * hotfix to the "loss of a variable if pushed to the stack while trying to push to reg" problem (can't return 2 ints)
 	 * @param reg the reg num in question if it's empty or not
 	 * @return true if the given reg number is empty (0)
-	 */
+	 
 	public boolean isRegEmpty(int reg){
 		int tmp = stack.get(reg);
 		if(tmp == 0){
@@ -151,7 +319,7 @@ public class RegStack {
 	 * 
 	 * @param val to go onto the stack
 	 * @return the location onto the stack
-	 */
+	 
 	public int pushToStack(int val){
 		int i = regNum; 
 		//checks for free spots on stack
@@ -198,7 +366,7 @@ public class RegStack {
 	/** 
 	 * if you want a guarantee that reg will not auto pop
 	 * @param reg
-	 */
+	 
 	public void protectReg(int reg){
 		if(reg < regNum)
 			lastUsed.set(reg, 0);
@@ -208,9 +376,8 @@ public class RegStack {
 	 * allows reg to pop
 	 * ::CAUTION:: can cause the base or stack pointers to be overwritten automatically
 	 * @param reg
-	 */
+	 
 	public void unprotectReg(int reg){
 		if(reg < regNum)
 			lastUsed.set(reg, 1);
-	}
-}
+	}*/
