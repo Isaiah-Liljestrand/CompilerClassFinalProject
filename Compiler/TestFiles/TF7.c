e8453a9c793be7cf0c387e2749dd5c3e8465125d
biopox <benjamin.fowler@student.nmt.edu>
2019-04-30 16:34:53 -0600

    Update TF7.c
    I can code c I swear

--- Compiler/TestFiles/TF7.c
+++ Compiler/TestFiles/TF7.c
@@ -1,8 +1,8 @@
 int foo(int i0, int i1, int i2, int i3, int i4, int i5, int i6, int i7, int i8, int i9, int i10, int i11, int i12, int i13, int i14, int i15, int i16, int i17, int i18, int i19){
-  int i = i0 + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18 + i19;
-  return i
+  int i = (i0 + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18 + i19);
+  return i;
 }
 
 int main(){
-  return foo(i);
+  return foo(0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9);
 }
